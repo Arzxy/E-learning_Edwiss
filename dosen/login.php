d5e6d0823f9875599d07589ff456cbbba236a18c
Arzxy <akmalrizki0102@gmail.com>
2024-08-29 20:06:56 +0700

    Baru lagi

--- dosen/login.php
+++ dosen/login.php
@@ -34,12 +34,12 @@ if (isset($_POST['submit'])) {
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="assets/modules/bootstrap-social/bootstrap-social.css">
-  <link rel="stylesheet" href="assets/modules/izitoast/css/iziToast.min.css">
+  <link rel="stylesheet" href="../admin/assets/modules/bootstrap-social/bootstrap-social.css">
+  <link rel="stylesheet" href="../admin/assets/modules/izitoast/css/iziToast.min.css">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/components.css">
+  <link rel="stylesheet" href="../admin/assets/css/components.css">
 </head>
 
 <body>
@@ -114,10 +114,10 @@ if (isset($_POST['submit'])) {
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="assets/js/stisla.js"></script>
+  <script src="../admin/assets/js/stisla.js"></script>
 
   <!-- JS Libraies -->
-  <script src="assets/modules/izitoast/js/iziToast.min.js"></script>
+  <script src="../admin/assets/modules/izitoast/js/iziToast.min.js"></script>
 
   <!-- Page Specific JS File -->
   <?php
@@ -152,8 +152,8 @@ if (isset($_POST['submit'])) {
   ?>
   
   <!-- Template JS File -->
-  <script src="assets/js/scripts.js"></script>
-  <script src="assets/js/custom.js"></script>
+  <script src="../admin/assets/js/scripts.js"></script>
+  <script src="../admin/assets/js/custom.js"></script>
 
   <!-- Page Specific JS File -->
 </body>
